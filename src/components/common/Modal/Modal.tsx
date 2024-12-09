24196bb9d631ff789c0315989b7593ace4b3ee0e
oweaj <qwe1520156@naver.com>
2024-12-09 15:28:38 +0900

    fix: 모달 컴포넌트 type 별 옵션 수정

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -11,57 +11,51 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
-import { MODAL_PAGE, MODAL_BUTTON, MODAL_TEXT, ModalPage, ModalButton, ModalText } from "@/types/common/modal";
+import { MODAL_INFO, ModalType } from "@/types/common/modal";
 import clsx from "clsx";
 import { useState } from "react";
 
 interface ModalProps {
-  size: "sm" | "md" | "lg";
-  title?: ModalPage;
-  description?: ModalText;
+  type: ModalType;
   content?: React.ReactNode;
-  checkBtn?: ModalButton;
-  activeButton: React.ReactNode;
   onSubmit?: () => void;
 }
 
-const MODAL_SIZE = {
-  sm: "max-w-[25rem] h-52",
-  md: "max-w-[30rem] h-[40rem]",
-  lg: "max-w-[60rem] max-lg:max-w-[40rem] max-sm:max-w-[30rem] max-2xl:h-[40rem] h-[45rem]",
-};
-
 export function Modal(props: ModalProps) {
-  const { size, title, description, content, checkBtn, activeButton, onSubmit } = props;
+  const { type, content, onSubmit } = props;
   const [open, setOpen] = useState(false);
   const handleClose = () => setOpen(false);
 
+  const modalData = MODAL_INFO[type];
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>{activeButton}</DialogTrigger>
+      <DialogTrigger asChild>
+        <Button>{modalData.trigger_btn}</Button>
+      </DialogTrigger>
       <DialogContent
-        className={clsx("flex flex-col items-center justify-center gap-8 rounded-lg", MODAL_SIZE[size])}
+        className={clsx("flex flex-col items-center justify-center gap-8 rounded-lg", modalData.size)}
         onPointerDownOutside={(e) => e.preventDefault()}
       >
         <DialogHeader>
           <DialogTitle className="absolute left-4 top-4 text-sm">
-            {title ? MODAL_PAGE[title] : <VisuallyHidden>제목 없음</VisuallyHidden>}
+            {modalData.title ? modalData.title : <VisuallyHidden>제목 없음</VisuallyHidden>}
           </DialogTitle>
           <DialogDescription className="text-base text-black">
-            {description ? MODAL_TEXT[description] : <VisuallyHidden>설명 없음</VisuallyHidden>}
+            {modalData.description ? modalData.description : <VisuallyHidden>설명 없음</VisuallyHidden>}
           </DialogDescription>
         </DialogHeader>
         {content && <div className="flex-grow overflow-y-auto scrollbar-hide">{content}</div>}
-        {title !== "MEMBER_LIST" && (
+        {modalData.title !== "맴버 리스트" && (
           <DialogFooter className="flex flex-row items-center justify-center gap-4">
-            {(checkBtn === "CHECK" || checkBtn === "REVIEW_SAVE") && (
-              <Button type="submit" variant="outline" onClick={handleClose}>
-                취소
+            <Button type="submit" variant="outline" onClick={handleClose}>
+              취소
+            </Button>
+            {modalData.submit_btn && (
+              <Button type="submit" onClick={onSubmit}>
+                {modalData.submit_btn}
               </Button>
             )}
-            <Button type="submit" onClick={onSubmit}>
-              {checkBtn && MODAL_BUTTON[checkBtn]}
-            </Button>
           </DialogFooter>
         )}
       </DialogContent>
--- src/types/common/modal.ts
+++ src/types/common/modal.ts
@@ -1,21 +1,46 @@
-export const MODAL_PAGE = {
-  MEMBER_LIST: "맴버 리스트",
-  PROFILE_EDIT: "프로필 수정",
-  REVIEW: "리뷰 쓰기",
-} as const;
+export type ModalType = "apply" | "cancle" | "delete" | "member" | "review" | "profile";
+type ModalOption = "size" | "trigger_btn" | "title" | "description" | "submit_btn";
 
-export const MODAL_BUTTON = {
-  CHECK: "확인",
-  REVIEW_SAVE: "리뷰 등록",
-  PROFILE_SAVE: "변경 내용 저장",
-} as const;
+const MODAL_SIZE = {
+  sm: "max-w-[25rem] h-52",
+  md: "max-w-[30rem] h-[40rem]",
+  lg: "max-w-[60rem] max-lg:max-w-[40rem] max-sm:max-w-[30rem] max-2xl:h-[40rem] h-[45rem]",
+};
 
-export const MODAL_TEXT = {
-  GATHERING_CANCLE: "참여한 모임을 취소하시겠습니까?",
-  GATHERING_DELETE: "정말 모임을 삭제하시겠습니까?",
-  MEMBER_REMOVE: "해당 맴버를 모임에서 제외하시겠습니까?",
-} as const;
-
-export type ModalPage = keyof typeof MODAL_PAGE;
-export type ModalButton = keyof typeof MODAL_BUTTON;
-export type ModalText = keyof typeof MODAL_TEXT;
+export const MODAL_INFO: Record<ModalType, Partial<Record<ModalOption, string>>> = {
+  apply: {
+    size: MODAL_SIZE.sm,
+    trigger_btn: "모임 신청 하기",
+    description: "해당 모임을 신청하시겠습니까?",
+    submit_btn: "확인",
+  },
+  cancle: {
+    size: MODAL_SIZE.sm,
+    trigger_btn: "모임 취소 하기",
+    description: "참여한 모임을 취소하시겠습니까?",
+    submit_btn: "확인",
+  },
+  delete: {
+    size: MODAL_SIZE.sm,
+    trigger_btn: "모임 삭제 하기",
+    description: "정말 모임을 삭제하시겠습니까?",
+    submit_btn: "확인",
+  },
+  member: {
+    size: MODAL_SIZE.md,
+    trigger_btn: "맴버 더보기",
+    title: "맴버 리스트",
+  },
+  review: {
+    size: MODAL_SIZE.md,
+    title: "리뷰 작성",
+    trigger_btn: "리뷰 작성 하기",
+    submit_btn: "리뷰 등록",
+  },
+  profile: {
+    size: MODAL_SIZE.lg,
+    title: "프로필 수정",
+    trigger_btn: "프로필 수정",
+    submit_btn: "변경 내용 저장",
+  },
+};
