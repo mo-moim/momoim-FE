8e4b5be8f49922eb059b70c10505d05215278445
oweaj <qwe1520156@naver.com>
2024-12-13 10:27:06 +0900

    fix: ui 작업시 props 옵셔널

--- src/components/common/select/Select.tsx
+++ src/components/common/select/Select.tsx
@@ -11,18 +11,21 @@ import clsx from "clsx";
 interface SelectProps {
   size?: string;
   data: readonly { value: string; label: string }[];
-  value: string;
-  defaultValue?: string;
+  value?: string;
   placeholder?: string;
   firstItem?: boolean;
-  onChange: (value: string) => void;
+  onChange?: (value: string) => void;
 }
 
-export function Select({ size, data, value, defaultValue, placeholder, firstItem, onChange }: SelectProps) {
+export function Select({ size, data, value, placeholder, firstItem, onChange }: SelectProps) {
   const firstItemFilter = firstItem ? data.slice(1) : data;
 
   return (
-    <SelectWrap value={value} defaultValue={placeholder ? defaultValue : data[0].value} onValueChange={onChange}>
+    <SelectWrap
+      value={value}
+      defaultValue={firstItem ? firstItemFilter[0].value : data[0].value}
+      onValueChange={onChange}
+    >
       <SelectTrigger className={clsx("w-24 border-gray-400", size)}>
         <SelectValue placeholder={placeholder} />
       </SelectTrigger>
