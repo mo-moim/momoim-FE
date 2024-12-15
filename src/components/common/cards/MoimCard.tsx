9d6dd448dcf6d876c587806f19cab4f6c76c6e5e
thbrthbr <tarzenezrat@gmail.com>
2024-12-15 21:01:21 +0900

    fix: 개더링컨텐트 인터페이스 수정 및 카드 사이즈 오류 수정

--- src/components/common/cards/MoimCard.tsx
+++ src/components/common/cards/MoimCard.tsx
@@ -1,16 +1,14 @@
 // type 종류
-// home | detail | mypage
+// home | mypage
+
+"use client";
 
 import ProgressBar from "@/components/common/ProgressBar";
 import { dateFormatter } from "@/lib/dateFormatter";
-import { leftTimeGenerator } from "@/lib/leftTimeGenerator";
 import { GatheringContent } from "@/types/common/gatheringContent";
-import { Members } from "@/types/common/members";
 import Image from "next/image";
 import Logo from "@/assets/svg/logo.svg";
-import DefaultProfile from "../../../assets/svg/default-profile.svg";
 import LocalIcon from "../../../assets/svg/geography_map_solid.svg";
-import Person from "../../../assets/svg/person.svg";
 import Heart from "../Heart";
 import Chip from "../Chip";
 
@@ -19,156 +17,18 @@ interface Props {
   data: GatheringContent;
   isWishList: boolean;
   likeTask?: () => void;
-  members?: Members[];
   customOnClick?: () => void;
-  customButton?: React.ReactNode;
 }
 
-export default function MoimCard({ type, data, members, customButton, customOnClick, isWishList, likeTask }: Props) {
+export default function MoimCard({ type, data, customOnClick, isWishList, likeTask }: Props) {
   const handleClickToEnter = customOnClick || (() => {});
   const handleLike = likeTask || (() => {});
-
-  if (type === "detail") {
-    return (
-      <div className="flex flex-col items-center gap-4 p-4 blg:w-[1100px]">
-        <div className="flex w-full flex-col items-center justify-center sm:flex-row">
-          <div className="relative mx-2 flex h-60 w-full max-w-[375px] items-center justify-center overflow-hidden rounded-[20px] border-2 border-solid border-gray-200 sm:max-w-60">
-            {data?.image ? <Image alt="thumbnail" src={data?.image} layout="fill" objectFit="contain" /> : <Logo />}
-          </div>
-          <div className="flex w-full items-center justify-center sm:justify-between">
-            <div className="flex w-full max-w-[375px] flex-col gap-4 p-2 sm:max-w-full">
-              <div className="w-full text-start font-bold text-main">{data?.category}</div>
-              <div className="flex flex-col gap-1">
-                <div className="flex w-full justify-start">
-                  <div className="text-overflow text-[26px] font-bold sm:text-[32px]">{data?.name}</div>
-                </div>
-                <div className="flex w-full justify-start gap-1 text-xs text-gray-700">
-                  <div className="overflow-hidden text-ellipsis whitespace-nowrap">{data?.subCategory}</div>
-                  <div>·</div>
-                  <div className="flex">
-                    <div className="flex items-center">
-                      <LocalIcon />
-                    </div>
-                    <div className="max-w-11 overflow-hidden text-ellipsis whitespace-nowrap xs:max-w-16 md:max-w-full">
-                      {data?.location}
-                    </div>
-                  </div>
-                  <div>·</div>
-                  <div>{dateFormatter(data?.nextGatheringAt as string).simple}</div>
-                  <div>·</div>
-                  <div className="flex">
-                    <div className="flex">
-                      <Person />
-                    </div>
-                    {data?.participantCount}/{data?.capacity}
-                  </div>
-                </div>
-                <div className="flex gap-1 text-sm">
-                  {[data?.status, data?.gatheringType, data?.isPeriodic].map((each, idx) => {
-                    const key = `chip:${data?.id}:${idx}`;
-                    if (typeof each === "boolean") {
-                      return each ? <Chip key={key} each="REGULAR" /> : null;
-                    }
-                    return <Chip key={key} each={each} />;
-                  })}
-                </div>
-              </div>
-              <div className="flex items-center justify-between gap-2">
-                <div className="flex items-center gap-1">
-                  <div className="relative flex h-8 w-8 items-center overflow-hidden rounded-full">
-                    {data?.managerProfileImage ? (
-                      <Image
-                        layout="fill"
-                        objectFit="contain"
-                        alt="managerProfileImage"
-                        src={data?.managerProfileImage}
-                      />
-                    ) : (
-                      <DefaultProfile />
-                    )}
-                  </div>
-                  <div>{data?.managerName}</div>
-                </div>
-                {/* 멤버창 컴포넌트 별도제작시 아래 div 대신 투입 - 1 */}
-                <div className="flex gap-2.5 lg:hidden">
-                  <div className="flex gap-2.5">
-                    {members?.map((member, idx) => {
-                      if (idx >= 3) return null;
-                      return (
-                        <div key={member?.userId} className="relative flex h-[34px] w-[34px] items-center rounded-full">
-                          {member.profileImage ? (
-                            <Image alt="member-image" layout="fill" objectFit="contain" src={member.profileImage} />
-                          ) : (
-                            <DefaultProfile />
-                          )}
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <button type="button">...</button>
-                </div>
-                {/* @@@ */}
-              </div>
-            </div>
-            <div className="hidden h-full w-[318px] flex-col gap-6 px-4 lg:flex">
-              {/* 멤버창 컴포넌트 제작시 아래 div 대신 투입 - 2 */}
-              <div className="flex gap-2.5">
-                <div className="flex gap-2.5">
-                  {members?.map((member, idx) => {
-                    if (idx >= 5) return null;
-                    return (
-                      <div key={member?.userId} className="relative flex h-[34px] w-[34px] items-center rounded-[50%]">
-                        {member.profileImage ? (
-                          <Image alt="member-image" layout="fill" objectFit="contain" src={member.profileImage} />
-                        ) : (
-                          <DefaultProfile />
-                        )}
-                      </div>
-                    );
-                  })}
-                </div>
-                <button type="button">...</button>
-              </div>
-              {/* @@@ */}
-              <div className="hidden h-full justify-center lg:flex">
-                <div className="w-full">
-                  <div className="text-gray-500">남은 시간</div>
-                  <div>
-                    <span className="text-[32px] font-bold">
-                      {leftTimeGenerator(data?.nextGatheringAt as string).days}
-                    </span>
-                    일{" "}
-                    <span className="text-[32px] font-bold">
-                      {leftTimeGenerator(data?.nextGatheringAt as string).hours}
-                    </span>
-                    시간
-                  </div>
-                </div>
-              </div>
-              {customButton}
-            </div>
-          </div>
-        </div>
-        <div className="flex w-full max-w-[375px] flex-col items-center justify-center px-2 sm:max-w-full lg:hidden">
-          <div className="w-full">
-            <div className="text-gray-500">남은 시간</div>
-            <div>
-              <span className="text-[32px] font-bold">{leftTimeGenerator(data?.nextGatheringAt as string).days}</span>일{" "}
-              <span className="text-[32px] font-bold">{leftTimeGenerator(data?.nextGatheringAt as string).hours}</span>
-              시간
-            </div>
-          </div>
-          {customButton}
-        </div>
-      </div>
-    );
-  }
 
   if (type === "home") {
     return (
       <button
         type="button"
-        className="flex w-[366px] flex-col items-center justify-center bg-white p-4"
+        className="flex max-w-[366px] flex-col items-center justify-center bg-white p-4"
         onClick={handleClickToEnter}
       >
         <div className="relative flex h-60 w-full items-center justify-center overflow-hidden rounded-[20px] bg-gray-100">
@@ -208,7 +68,7 @@ export default function MoimCard({ type, data, members, customButton, customOnCl
             <div>
               <div className="flex gap-1 text-sm">
                 {[data?.status, data?.gatheringType, data?.isPeriodic].map((each, idx) => {
-                  const key = `chip:${data?.id}:${idx}`;
+                  const key = `chip:${data?.gatheringId}:${idx}`;
                   if (typeof each === "boolean") {
                     return each ? <Chip key={key} each="REGULAR" /> : null;
                   }
@@ -234,10 +94,10 @@ export default function MoimCard({ type, data, members, customButton, customOnCl
     return (
       <button
         type="button"
-        className="flex w-full max-w-[1100px] items-start gap-2 sm:items-center"
+        className="flex w-full max-w-[1100px] items-start gap-2 py-2 sm:items-center"
         onClick={handleClickToEnter}
       >
-        <div className="relative flex aspect-square h-[40%] w-[40%] min-w-20 max-w-40 items-center justify-center overflow-hidden rounded-[20px] border-2 border-solid border-gray-200 sm:w-40 sm:min-w-40">
+        <div className="relative flex aspect-square h-[30%] w-[30%] items-center justify-center overflow-hidden rounded-[20px] border-2 border-solid border-gray-200 sm:min-h-40 sm:w-40 sm:min-w-40">
           {["CANCELED", "FINISHED"].includes(data?.status as string) ? (
             <div className="absolute flex h-full w-full items-center justify-center bg-[rgba(0,0,0,0.4)] text-center text-white">
               {{
@@ -277,7 +137,7 @@ export default function MoimCard({ type, data, members, customButton, customOnCl
             </div>
             <div className="flex gap-1 text-sm">
               {[data?.status, data?.gatheringType, data?.isPeriodic].map((each, idx) => {
-                const key = `chip:${data?.id}:${idx}`;
+                const key = `chip:${data?.gatheringId}:${idx}`;
                 if (typeof each === "boolean") {
                   return each ? <Chip key={key} each="REGULAR" /> : null;
                 }
--- src/types/common/gatheringContent.ts
+++ src/types/common/gatheringContent.ts
@@ -1,21 +1,26 @@
+import { Members } from "./members";
+
 export interface GatheringContent {
-  id: number;
-  managerId: number;
-  managerName: string;
-  managerProfileImage: string;
+  gatheringId?: number; // 홈
+  id?: number; // 디테일
+  managerId?: number; // 디테일
+  managerName?: string; // 디테일
+  managerProfileImage?: string; // 디테일
   category: string;
   subCategory: string;
   name: string;
   gatheringType: string;
   status: string;
   image: string;
-  description: string;
-  address: string;
+  description?: string; // 디테일
+  address?: string; // 디테일
   tags: string[];
   location: string;
   capacity: number;
   participantCount: number;
+  isWishList?: boolean; // 홈
   isPeriodic: boolean;
   nextGatheringAt: string;
-  wishlistCount: number;
+  wishlistCount?: number; // 홈
+  members?: Members[]; // 홈
 }
