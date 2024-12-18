90c1a08dec2f2d4ad46587e2d10065e4e88c94f6
oweaj <qwe1520156@naver.com>
2024-12-18 18:00:32 +0900

    feat: 모달 전역 상태관리

---
+++ src/store/modalStore.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+interface ModalStoreType {
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+export const useModalStore = create<ModalStoreType>((set) => ({
+  isOpen: false,
+  openModal: () => set({ isOpen: true }),
+  closeModal: () => set({ isOpen: false }),
+}));
