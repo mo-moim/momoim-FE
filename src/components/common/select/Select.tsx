7a9849f196071dbf340f62d4cb72a74dd2b35e63
oweaj <qwe1520156@naver.com>
2024-12-18 18:01:12 +0900

    fix: 상수 데이터 변경에 따른 props 삭제

--- src/components/common/select/Select.tsx
+++ src/components/common/select/Select.tsx
@@ -13,40 +13,22 @@ interface SelectProps {
   data: readonly { value: string; label: string }[];
   value?: string;
   placeholder?: string;
-  firstItem?: boolean;
   onChange?: (value: string) => void;
 }
 
-export function Select({ size, data, value, placeholder, firstItem, onChange }: SelectProps) {
-  const firstItemFilter = firstItem ? data.slice(1) : data;
-  let defaultValue;
-
-  if (firstItem) {
-    defaultValue = firstItemFilter[0].value;
-  } else if (placeholder) {
-    defaultValue = "";
-  } else {
-    defaultValue = data[0].value;
-  }
-
+export function Select({ size, data, value, placeholder, onChange }: SelectProps) {
   return (
-    <SelectWrap value={value} defaultValue={defaultValue} onValueChange={onChange}>
+    <SelectWrap value={value} onValueChange={onChange}>
       <SelectTrigger className={clsx("w-24 border-gray-400", size)}>
         <SelectValue placeholder={placeholder} />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
-          {firstItem
-            ? firstItemFilter.map((select) => (
-                <SelectItem key={select.value} value={select.value}>
-                  {select.label}
-                </SelectItem>
-              ))
-            : data.map((select) => (
-                <SelectItem key={select.value} value={select.value}>
-                  {select.label}
-                </SelectItem>
-              ))}
+          {data.map((select) => (
+            <SelectItem key={select.value} value={select.value}>
+              {select.label}
+            </SelectItem>
+          ))}
         </SelectGroup>
       </SelectContent>
     </SelectWrap>
