4a9664bb6ab568c24cc983a0a2a1e052f3203cda
주승연 <50696162+juseungyeon@users.noreply.github.com>
2024-12-18 14:15:22 +0900

    fix: suspense 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { Suspense } from "react";
 import MoimPage from "./_component/MoimPage";
 
 export default function Home() {
-  return <MoimPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MoimPage />
+    </Suspense>
+  );
 }
--- src/app/[category]/page.tsx
+++
@@ -1,5 +0,0 @@
-import MoimPage from "../(main)/_component/MoimPage";
-
-export default function CategoryPage({ params }: { params: { category: string } }) {
-  return <MoimPage initialCategory={params.category.toUpperCase()} />;
-}
