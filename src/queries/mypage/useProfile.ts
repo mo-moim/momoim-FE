e9ff6d8f79542c8278edb7e34dfab0a449a39a46
thbrthbr <tarzenezrat@gmail.com>
2024-12-26 19:17:49 +0900

    fix: useProfile 삭제

--- src/queries/mypage/useProfile.ts
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUser } from "../../api/user";
-
-export const useProfile = () => {
-  return useQuery({ queryKey: ["profile"], queryFn: getUser });
-};
