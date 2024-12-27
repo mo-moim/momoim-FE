29c8da660a26a6cfe1d7c2a2b6da0bb01b2c0d13
oweaj <qwe1520156@naver.com>
2024-12-27 18:19:38 +0900

    fix: 모달 props 및 상태 수정

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -10,27 +10,22 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useModalStore } from "@/store/modalStore";
 import clsx from "clsx";
 
 interface ModalProps {
   size?: string;
-  title: string;
+  title?: string;
+  open: boolean;
+  action: (open: boolean) => void;
   triggerButton: React.ReactNode;
   content: React.ReactNode;
   showFooter?: boolean;
   onSubmit?: () => void;
 }
 
-export function Modal({ size, title, triggerButton, content, showFooter = true, onSubmit }: ModalProps) {
-  const { modalState, openModal, closeModal } = useModalStore();
-
-  const handleModalStateChange = (isOpen: boolean) => {
-    return isOpen ? openModal(title, content) : closeModal(title);
-  };
-
+export function Modal({ size, title, open, action, triggerButton, content, showFooter = true, onSubmit }: ModalProps) {
   return (
-    <Dialog open={modalState[title]?.isOpen} onOpenChange={(isOpen) => handleModalStateChange(isOpen)}>
+    <Dialog open={open} onOpenChange={action}>
       <DialogTrigger asChild>{triggerButton}</DialogTrigger>
       <DialogContent
         className={clsx("h-1/5 min-h-36 w-[25rem] gap-6 rounded-lg p-4", size)}
@@ -44,7 +39,7 @@ export function Modal({ size, title, triggerButton, content, showFooter = true, 
         </DialogDescription>
         {showFooter && (
           <DialogFooter className="flex w-full flex-row items-center justify-center gap-2 sm:gap-0">
-            <Button type="button" variant="outline" className="w-full" onClick={() => closeModal(title)}>
+            <Button type="button" variant="outline" className="w-full" onClick={() => action(false)}>
               취소
             </Button>
             <Button type="submit" onClick={onSubmit} className="w-full">
