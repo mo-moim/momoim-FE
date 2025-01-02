dec117bf4da5eb2da23ef3795ceb51f15ceedb1f
oweaj <qwe1520156@naver.com>
2025-01-02 14:28:00 +0900

    fix: 로그인 상태에 따른 toast 추후 변경 예정

--- src/queries/gatherings/useGatheringJoin.ts
+++ src/queries/gatherings/useGatheringJoin.ts
@@ -4,6 +4,7 @@ import { gatheringJoinApi } from "@/api/gatherings";
 
 export const useGatheringJoin = () => {
   const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: gatheringJoinApi,
     onSuccess: (id: number) => {
@@ -15,12 +16,20 @@ export const useGatheringJoin = () => {
       queryClient.invalidateQueries({ queryKey: ["gatheringDetail", id] });
     },
     onError: (error: any) => {
-      toast({
-        variant: "destructive",
-        title: "모임 신청 실패",
-        description: error.response?.data?.message || "알 수 없는 오류가 발생했습니다.",
-        duration: 2000,
-      });
+      if (error.response?.status === 403) {
+        toast({
+          title: "🖐️ 로그인 필요",
+          description: "해당 서비스 이용은 로그인이 필요합니다.",
+          duration: 2000,
+        });
+      } else {
+        toast({
+          variant: "destructive",
+          title: "모임 신청 실패",
+          description: error.response?.data?.message || "알 수 없는 오류가 발생했습니다.",
+          duration: 2000,
+        });
+      }
     },
   });
 };
