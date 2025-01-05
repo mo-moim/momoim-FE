e670322628a38ec03a09da142c69d50366cc8620
주승연 <50696162+juseungyeon@users.noreply.github.com>
2025-01-05 18:40:18 +0900

    fix: 로그아웃 시 캐시 초기화 방식 개선

--- src/queries/auth/useLogout.ts
+++ src/queries/auth/useLogout.ts
@@ -1,20 +1,16 @@
 import { logoutApi } from "@/api/auth/auth";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 
 export const useLogout = () => {
-  const router = useRouter();
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: logoutApi,
     onSuccess: () => {
-      queryClient.refetchQueries({ queryKey: ["gatherings"] });
-      queryClient.setQueryData(["user"], null);
-      queryClient.removeQueries({ queryKey: ["user"] });
-      router.push("/");
+      queryClient.clear();
       Cookies.remove("accessToken");
+      window.location.href = "/";
     },
   });
 };
